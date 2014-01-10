389be3393736fb3ef8b60c75188578d4e3a7f4e3
Nick Rawe <nrawe@reapit.com>
2014-01-10 17:33:18 +0000

    Started to rework test suite to match new API.

--- tests/Micro/Tests/TestCase.php
+++ tests/Micro/Tests/TestCase.php
@@ -1,28 +1,6 @@
 <?php
 namespace Micro\Tests;
 
-abstract class TestCase extends \PHPUnit_Framework_TestCase
+class TestCase extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @return \Micro\Application
-     */
-    protected function getApp()
-    {
-        return new \Micro\Application(new \Micro\DefaultEnvironment());
-    }
-    
-    /**
-     * @return \Micro\Request
-     */
-    protected function getReq()
-    {
-        return \Micro\Request::create(
-                "/", 
-                "GET", 
-                array(),
-                array(),
-                array(),
-                array("CONTENT_TYPE" => "text/html")
-        );
-    }
 }
