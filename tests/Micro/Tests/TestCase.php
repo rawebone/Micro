7ad56f14f5445e22bc1cfc23c898caf2a0f4e8da
rawebone <rawebone@gmail.com>
2014-01-12 22:56:40 +0000

    Added the ability to use Prophecy in the TestCases.

--- tests/Micro/Tests/TestCase.php
+++ tests/Micro/Tests/TestCase.php
@@ -1,6 +1,24 @@
 <?php
 namespace Micro\Tests;
 
+use Prophecy\Prophet;
+
 class TestCase extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * The prophecy instance to be used for mocking purposes.
+     *
+     * @var \Prophecy\Prophet
+     */
+    protected $prophet;
+    
+    public function setUp()
+    {
+        $this->prophet = $this->getProphet();
+    }
+    
+    protected function getProphet()
+    {
+        return new Prophet();
+    }
 }
