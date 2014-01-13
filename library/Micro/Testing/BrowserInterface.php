e9f0ce66b09cddf06075b4b7c6a2d3af2314ea30
Nick Rawe <nrawe@reapit.com>
2014-01-13 10:54:40 +0000

    Updated documentation of the BrowserInterface

--- library/Micro/Testing/BrowserInterface.php
+++ library/Micro/Testing/BrowserInterface.php
@@ -5,7 +5,11 @@ use Micro\Application;
 
 /**
  * The BrowserInterface provides a mechanism for interacting with a Micro
- * Application through a clean API largely based off of Kris Wallsmiths Buzz.
+ * Application through a clean API largely based off of Kris Wallsmiths' Buzz.
+ * 
+ * It's important to note, given that this works over the top of the Symfony
+ * HttpFoundation, that passed headers must be in the PHP format, i.e.
+ * Content-Type becomes CONTENT_TYPE etc, Accepts becomes HTTP_ACCEPTS.
  */
 interface BrowserInterface
 {
@@ -17,49 +21,49 @@ interface BrowserInterface
     /**
      * Performs a GET request against the application with the specified URI,
      * 
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return \Symfony\Component\HttpFoundation\Response|false
      */
     function get($uri, array $headers = array());
     
     /**
      * Performs a POST request against the application with the specified URI,
      * 
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return \Symfony\Component\HttpFoundation\Response|false
      */
     function post($uri, array $headers = array(), $content = "");
     
     /**
      * Performs a HEAD request against the application with the specified URI.
      * 
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return \Symfony\Component\HttpFoundation\Response|false
      */
     function head($uri, array $headers = array());
     
     /**
      * Performs a PATCH request against the application with the specified URI.
      * 
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return \Symfony\Component\HttpFoundation\Response|false
      */
     function patch($uri, array $headers = array(), $content = "");
     
     /**
      * Performs a PUT request against the application with the specified URI.
      * 
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return \Symfony\Component\HttpFoundation\Response|false
      */
     function put($uri, array $headers = array(), $content = "");
     
     /**
      * Performs a DELETE request against the application with the specified URI.
      * 
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return \Symfony\Component\HttpFoundation\Response|false
      */
     function delete($uri, array $headers = array(), $content = "");
     
     /**
      * Performs an OPTIONS request against the application with the specified URI.
      * 
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return \Symfony\Component\HttpFoundation\Response|false
      */
     function options($uri, array $headers = array(), $content = "");
     
